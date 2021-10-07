a63e7a478cb83a1f3809c2190e6a392423d0b356
luiz.oliveira <luiz.oliveira@newm.com.br>
2021-10-07 13:04:07 -0300

    Upload imagens

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -28,15 +28,17 @@ class EventController extends Controller
         $event->description = $request->description;
 
         // Image Upload
-        if($request->hasFile('image') && $request->file('image')->isValid()){
+        if($request->hasFile('image') && $request->file('image')->isValid()) {
 
             $requestImage = $request->image;
 
             $extension = $requestImage->extension();
 
-            $imageName = md5($requestImage->image->getClientOriginalName() . strtotime("now"));
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
 
+            $requestImage->move(public_path('img/events'), $imageName);
 
+            $event->image = $imageName;
         }
 
         $event->save();
