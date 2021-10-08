be5f97d8d2e93b2bcbf60b4c91f04eb6fe351c14
luiz.oliveira <luiz.oliveira@newm.com.br>
2021-10-08 14:32:48 -0300

    Login function with jetstream e livewire

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -28,12 +28,26 @@
                         <li class="nav-item">
                             <a href="/events/create" class="nav-link">Criar Evento</a>
                         </li>
-                        <li class="nav-item">
-                            <a href="/" class="nav-link">Entrar</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/" class="nav-link">Cadastrar</a>
-                        </li>
+                        @guest
+                            <li class="nav-item">
+                                <a href="/login" class="nav-link">Entrar</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="/register" class="nav-link">Cadastrar</a>
+                            </li> 
+                        @endguest
+                        @auth
+                            <li class="nav-item">
+                                <a href="/dashbord" class="nav-link">Meus eventos</a>
+                            </li>
+                            <li class="nav-item">
+                                <form action="/logout" method="POST">
+                                    @csrf
+                                    <a href="/logout" class="nav-link" onclick="event.preventDefault();
+                                    this.closest('form').submit();">Sair</a>
+                                </form>
+                            </li>
+                        @endauth
                     </ul>
                 </div>
             </nav>
--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,7 @@ Route::post('/postevent', [EventController::class, 'store']);
 Route::get('/contato', function () {
     return view('contato');
 });
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
