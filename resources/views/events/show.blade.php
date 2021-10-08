27f19d071bffcfe934c9e16804e87d8f7ea5e8fe
luiz.oliveira <luiz.oliveira@newm.com.br>
2021-10-08 11:40:50 -0300

    Adicionado a data no show event

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -10,6 +10,7 @@
       <div id="info-container" class="col-md-6">
         <h1>{{ $event->title }}</h1>
         <p class="event-city"><ion-icon name="location-outline"></ion-icon> {{ $event->city }}</p>
+        <p class="event-date"><ion-icon name="calendar-outline"></ion-icon> {{ date('d/m/Y', strtotime($event->date)) }}</p>
         <p class="events-participants"><ion-icon name="people-outline"></ion-icon> X Participantes</p>
         <p class="event-owner"><ion-icon name="star-outline"></ion-icon> Dono do Evento</p>
         <a href="#" class="btn btn-primary" id="event-submit">Confirmar Presença</a>
