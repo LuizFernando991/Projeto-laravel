c9fd5fb3646306e24934113952171216efb3d2f6
luiz.oliveira <luiz.oliveira@newm.com.br>
2021-10-11 12:48:54 -0300

    Dashboard increment

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,17 +2,7 @@
 @section('title', 'Dasbord')
 
 @section('content')
-    
-    <div class="col-md-10 offset-md-1 dashbord-title-container">
-        <h1>Meus Eventos</h1>
-    </div>
-    <div class="col-md-10 offset-md-1 dashboard-events-container">
-        @if (count($events)>0)
-                   
-        @else
-            <p>Você ainda não tem eventos, <a href="/events/create">Criar evento</a></p>
-        @endif
-    </div>
+
 
 
 @endsection
